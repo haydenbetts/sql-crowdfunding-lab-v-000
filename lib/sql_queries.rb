817ac1fecc45195a1646ab7beba0d5387b73c2d3
Hayden Betts <haydenbetts@gmail.com>
2018-07-09 22:07:35 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -48,7 +48,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM pledges
   INNER JOIN projects
   ON pledges.project_id = projects.id
-  SELECT pledges.category = 'music';
+  WHERE pledges.category = 'music';
   "
 end
 
